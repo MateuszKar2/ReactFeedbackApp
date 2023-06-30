1a630dec2cf2691ed644f56934fd0eed5f39270f
MateuszKar2 <system505@protonmail.com>
2023-06-30 19:55:09 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Section } from './Section/Section';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import  FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import { Statistics} from './Statistics/Statistics';
 import { Notification} from './Notification/Notification';
 
